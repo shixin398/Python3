f9acc0852db0921877aa8eaab97f7a3e4454ed04
lishitao <li.shitao@chinaott.net>
2019-03-22 17:55:18 +0800

    add annotation

--- spider/spiderOne/spiderOne.py
+++ spider/spiderOne/spiderOne.py
@@ -10,7 +10,7 @@
 
 import urllib.request
 
-with urllib.request.urlopen('https://cn.bing.com/') as f:
-    print(f.read().decode('utf-8'))
+with urllib.request.urlopen('https://cn.bing.com/') as f: #加载必应网站
+    print(f.read().decode('utf-8')) #读取网址内容，读取的内容为2进制代码，根据网站编码格式，进行解码utf-8
 
 
